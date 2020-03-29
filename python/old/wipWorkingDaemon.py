3ac1ff3a640bb518e3ee3c50c8369bac27adfa3a
Chris Hubbard <chubbard@iwillfearnoevil.com>
2020-03-28 22:50:35 -0700

    working webserver with err correction

--- python/old/wipWorkingDaemon.py
+++ python/old/wipWorkingDaemon.py
@@ -73,82 +73,56 @@ class CustomServerHandler(http.server.BaseHTTPRequestHandler):
 #           We only support banip and unbanip for safety
 #           412 is precondition failed
             if httpBan not in ["banip","unbanip"]:
-                failed = 'true'
                 self.send_response(412)
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
+                failed = 'true'
                 response = {
-                    'result': "failed",
-                    'reason': "ban= supported values banip, unbanip"
+                    'manditory_parameter': "ban= only supports banip and unbanip"
                 }
-                self.wfile.write(bytes(json.dumps(response), 'utf-8'))
 
             try:
                 ipList = getvars.get('ip')
                 httpIp = ''.join(ipList)
             except:
-                failed = 'true'
                 self.send_response(412)
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
+                failed = 'true'
                 response = {
-                    'result': "failed",
-                    'reason': "ip=w.x.y.z is manditory"
+                    'manditory_parameter': "ip=w.x.y.z is manditory"
                 }
-                self.wfile.write(bytes(json.dumps(response), 'utf-8'))
 
 #           If we have what we need, do something now
 #           HTTP 202 is accepted
             if failed != 'true':
                 command = 'fail2ban-client set ' + httpJail + ' ' + httpBan + ' ' + httpIp
+                self.send_response(202)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+ #               os.system(command)
+                command2 = ["fail2ban-client", "set", httpJail, httpBan, httpIp]
+#                runUpdate = subprocess.run(["fail2ban-client", "set", httpJail, httpBan, httpIp],shell=true)
+#                runUpdate = subprocess.getoutput(command2,shell=True)
                 runUpdate = subprocess.Popen(command,shell=True, stdout=subprocess.PIPE)
                 (runUpdateOutput, err) = runUpdate.communicate()
                 runUpdate_status = runUpdate.wait()
-#                if ( (runUpdate_status == "255") and ("is not banned" not in runUpdateOutput)):
-                if ( "is not banned" in str(runUpdateOutput)):
-                    self.send_response(200)
-                    self.send_header('Content-type', 'application/json')
-                    self.end_headers()
-                    result = "success"
-                    reason = "IP was not banned to begin with"
-                elif ("Sorry" in str(runUpdateOutput)):
-                    self.send_response(417)
-                    self.send_header('Content-type', 'application/json')
-                    self.end_headers()
-                    reason = "jail does not exist"
-                    result = "failed"
-                else:
-                    result = "success"
-                    reason = "good data"
-                    self.send_response(202)
-                    self.send_header('Content-type', 'application/json')
-                    self.end_headers()
-
                 response = {
-                    'result': str(result),
-                    'reason': str(reason),
-                    'ip': str(httpIp)
-#                    'path': self.path,
-#                    'get_vars': str(getvars),
-#                    'conf_default_ban': str(ban),
-#                    'conf_default_jail': str(jail),
-#                    'conf_default_PORT': str(PORT),
-#                    'conf_default_USERNAME': str(USERNAME),
-#                    'conf_default_USERPASS': str(USERPASS),
-#                    'os_command': str(command),
-#                    'subprocess_command': str(command),
-#                    'subprocess_status': str(runUpdate_status),
-#                    'subprocess_output': str(runUpdateOutput),
-#                    'keys': str(keys),
-#                    'values': str(values),
-#                    'runUpdate': str(runUpdate)
+                    'path': self.path,
+                    'get_vars': str(getvars),
+                    'conf_default_ban': str(ban),
+                    'conf_default_jail': str(jail),
+                    'conf_default_PORT': str(PORT),
+                    'conf_default_USERNAME': str(USERNAME),
+                    'conf_default_USERPASS': str(USERPASS),
+                    'os_command': str(command),
+                    'subprocess_command': str(command2),
+                    'subprocess_status': str(runUpdate_status),
+                    'subprocess_output': str(runUpdateOutput),
+                    'keys': str(keys),
+                    'values': str(values),
+                    'runUpdate': str(runUpdate)
                 }
-                self.wfile.write(bytes(json.dumps(response), 'utf-8'))
-            elif (failed == 'true'):
-#           We already gave data back.  Stub
-#           for things like verbose results... Maybe
-                foo = "var"
-
 #           Even on a fatal error never give too much data
 #           when dealing with security "things"
             else:
@@ -156,12 +130,98 @@ class CustomServerHandler(http.server.BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
                 response = {
-                    'result': "failed",
-                    'path': self.path,
-                    'get_vars': str(getvars),
                     'error': "An unexpected error occured.  No further details available"
                 }
-                self.wfile.write(bytes(json.dumps(response), 'utf-8'))
+
+# This stuff should be removed or completely disable
+#            base_path = urlparse(self.path).path
+#            if base_path == '/path1':
+#                # Do some work
+#                pass
+#            elif base_path == '/path2':
+#                # Do some work
+#                pass
+
+#            self.wfile.write(bytes(json.dumps(response), 'utf-8'))
+#        else:
+#            self.do_AUTHHEAD()
+
+#            response = {
+#                'success': False,
+#                'error': 'Invalid credentials'
+#            }
+
+            self.wfile.write(bytes(json.dumps(response), 'utf-8'))
+
+    ''' There is no reason currently to support
+        POST so we are going to simply ignore this but
+        keep it stubbed just in case '''
+    def do_POST(self):
+        key = self.server.get_auth_key()
+
+        ''' Present frontpage with user authentication. '''
+        if self.headers.get('Authorization') == None:
+            self.do_AUTHHEAD()
+
+            response = {
+                'success': False,
+                'error': 'No auth header received'
+            }
+
+            self.wfile.write(bytes(json.dumps(response), 'utf-8'))
+
+        elif self.headers.get('Authorization') == 'Basic ' + str(key):
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+
+            postvars = self._parse_POST()
+            getvars = self._parse_GET()
+
+            response = {
+                'path': self.path,
+                'get_vars': str(getvars),
+                'get_vars': str(postvars)
+            }
+
+            base_path = urlparse(self.path).path
+            if base_path == '/path1':
+                # Do some work
+                pass
+            elif base_path == '/path2':
+                # Do some work
+                pass
+
+            self.wfile.write(bytes(json.dumps(response), 'utf-8'))
+        else:
+            self.do_AUTHHEAD()
+
+            response = {
+                'success': False,
+                'error': 'Invalid credentials'
+            }
+
+            self.wfile.write(bytes(json.dumps(response), 'utf-8'))
+
+        response = {
+            'path': self.path,
+            'get_vars': str(getvars),
+        }
+
+        self.wfile.write(bytes(json.dumps(response), 'utf-8'))
+
+    def _parse_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        if ctype == 'multipart/form-data':
+            postvars = cgi.parse_multipart(self.rfile, pdict)
+        elif ctype == 'application/x-www-form-urlencoded':
+            length = int(self.headers.getheader('content-length'))
+            postvars = cgi.parse_qs(
+                self.rfile.read(length), keep_blank_values=1)
+        else:
+            postvars = {}
+
+        return postvars
 
     def _parse_GET(self):
         getvars = parse_qs(urlparse(self.path).query)
---
+++ python/webserver.py
@@ -0,0 +1,6 @@
+jail='fail2ban-flies'
+ban='true'
+PORT = 3333
+USERNAME='changeme'
+USERPASS='test'
+
